7655e2ce23d3d93b89dc77a05babc91f4537ca1d
kaito834 <kaito834@users.noreply.github.com>
2015-08-09 02:02:30 +0900

    Added try-except block to handle urllib.error.HTTPError/URLError.

--- snippets/python/urllib-request_basicAuth.py
+++ snippets/python/urllib-request_basicAuth.py
@@ -4,6 +4,7 @@
 # Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
 
 import urllib.request
+import urllib.error
 import getpass
 
 # If you access to url below via Proxy,
@@ -30,9 +31,18 @@ urllib.request.install_opener(opener)
 
 # I can get http.client.HTTPResponse object in variable 'res'
 # https://docs.python.org/3/library/http.client.html#httpresponse-objects
-#
-# ToDo: Error Handling
-# https://docs.python.org/3/howto/urllib2.html#handling-exceptions
-res = urllib.request.urlopen(url)
-res_body = res.read()
-print(res_body.decode('utf-8'))
+try:
+    res = urllib.request.urlopen(url)
+    res_body = res.read()
+    print(res_body.decode('utf-8'))
+except urllib.error.HTTPError as e:
+    # https://docs.python.org/3/library/urllib.error.html#urllib.error.HTTPError
+    print('[!] urllib.error.HTTPError: {0}'.format(e))
+    if e.code == 401:
+        print('[!] User name or password on Basic Authentication is not correct.')
+except urllib.error.URLError as e:
+    # https://docs.python.org/3/library/urllib.error.html#urllib.error.URLError
+    # Confirmed that this was raised when I could not connect proxy server
+    print('[!] urllib.error.URLError: {0}'.format(e))
+except Exception as e:
+    print('[!] Exception: {0}'.format(e))
