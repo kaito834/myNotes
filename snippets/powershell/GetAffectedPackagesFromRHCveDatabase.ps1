22197c2cc2dce4c24a2135d4d2fb6e65ccb287dd
kaito834 <kaito834@users.noreply.github.com>
2016-02-17 12:15:05 +0900

    Moved some code into try block; Pipeline process continued when an exception would raise.

--- snippets/powershell/GetAffectedPackagesFromRHCveDatabase.ps1
+++ snippets/powershell/GetAffectedPackagesFromRHCveDatabase.ps1
@@ -20,31 +20,31 @@ begin{
 process{
   $RHCveDatabaseUrl = 'https://access.redhat.com/security/cve/' + $Cve
   $targetClass = 'field field-name-field-affected-state-package-txt field-type-text-long field-label-above '
+  Write-Host $RHCveDatabaseUrl
+  Write-Host '==='
 
   try{
     $res = Invoke-WebRequest $RHCveDatabaseUrl
+
+    # References
+    # https://superwidgets.wordpress.com/tag/read-html-via-powershell-powershell/
+    # http://www.leeholmes.com/blog/2015/01/05/extracting-tables-from-powershells-invoke-webrequest/
+    $divElement = $res.ParsedHtml.getElementsByTagName('div') | Where { $_.className -eq 'field field-name-field-affected-state-package-txt field-type-text-long field-label-above ' }
+    $trElements = $divElement.getElementsByTagName('tr')
+    foreach ( $trElement in $trElements ) {
+      $cells = @()
+      $trElement.getElementsByTagName('th') | select innerText | foreach { $cells += $_.innerText }
+      $trElement.getElementsByTagName('td') | select innerText | foreach { $cells += $_.innerText }
+      Write-Host -Separator ', ' $cells
+    }
   }
   catch{
     write-host "Caught an exception:" -ForegroundColor Red
     write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
     write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
-    exit 1
   }
 
-  Write-Host $RHCveDatabaseUrl
-  Write-Host '==='
-
-  # References
-  # https://superwidgets.wordpress.com/tag/read-html-via-powershell-powershell/
-  # http://www.leeholmes.com/blog/2015/01/05/extracting-tables-from-powershells-invoke-webrequest/
-  $divElement = $res.ParsedHtml.getElementsByTagName('div') | Where { $_.className -eq 'field field-name-field-affected-state-package-txt field-type-text-long field-label-above ' }
-  $trElements = $divElement.getElementsByTagName('tr')
-  foreach ( $trElement in $trElements ) {
-    $cells = @()
-    $trElement.getElementsByTagName('th') | select innerText | foreach { $cells += $_.innerText }
-    $trElement.getElementsByTagName('td') | select innerText | foreach { $cells += $_.innerText }
-    Write-Host -Separator ', ' $cells
-  }
+  Write-Host ''
 }
 
 end{}
