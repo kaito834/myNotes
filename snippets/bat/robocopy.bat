5fd2b6fc8db0e5e7612caade7e957dfb257bea67
kaito834 <kaito834@users.noreply.github.com>
2016-03-26 22:13:14 +0900

    Added the condition to run robocopy.

--- snippets/bat/robocopy.bat
+++ snippets/bat/robocopy.bat
@@ -1,3 +1,12 @@
 REM Copy local folder to remote folder
 REM I executed this batch on Windows 8.1
+set /P answer="Do you synchronize your data to remote host? (y/n): "
+if /i "%answer%" == "y" (
 robocopy "C:\Users\sample\Documents\mydata" "\\192.168.0.120\backup\" /E /FFT /R:3 /W:5 /NP /TEE /LOG+:"\\192.168.0.120\backup\robocopy.log"
+) else if /i "%answer%" == "n" (
+  echo ** Cancelled the synchronization.
+) else (
+  echo !! Invalid input. exit.
+)
+
+pause
