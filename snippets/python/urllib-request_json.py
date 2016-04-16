b20dd3d76ad4eeba4f38fd7c186858ed98130a11
kaito834 <kaito834@users.noreply.github.com>
2016-04-16 23:05:29 +0900

    Output indented JSON object.

--- snippets/python/urllib-request_json.py
+++ snippets/python/urllib-request_json.py
@@ -13,6 +13,7 @@ proxyhandler = urllib.request.ProxyHandler({'http': 'http://127.0.0.1:8080/', 'h
 opener = urllib.request.build_opener(proxyhandler)
 urllib.request.install_opener(opener)
 res = urllib.request.urlopen('https://ip-ranges.amazonaws.com/ip-ranges.json')
+# type(res_body) is class 'bytes'
 res_body = res.read()
 
 # By the way, you can use environment variables http_proxy or https_proxy when access web site/service via proxy.
@@ -20,7 +21,17 @@ res_body = res.read()
 # set https_proxy=https://127.0.0.1:8080/
 
 # https://docs.python.org/3/library/json.html
+# type(j) is class 'dict'
 j = json.loads(res_body.decode("utf-8"))
+
+# Output Escaped ASCII CR+LF if json.dumps() is outputed directly; print(json.dumps())
+# type(j_str) is class 'str'
+# Ref.
+# http://stackoverflow.com/questions/16318543/cant-pretty-print-json-from-python
+# http://stackoverflow.com/questions/9785049/python-how-to-use-json-dumps-on-windows
+j_str = json.dumps(j, ensure_ascii=True, sort_keys=True, indent=4)
+print(j_str)
+print('='*3)
 
 # parse strings: 'ip_prefix' and 'region'
 #for i in range(len(j['prefixes'])):
