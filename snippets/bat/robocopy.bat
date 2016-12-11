8ac5592570fa6398165cd5eca49a28a2d04375ac
kaito834 <kaito834@users.noreply.github.com>
2016-12-11 13:40:22 +0900

    Added note for the specification of robocopy as comment.

--- snippets/bat/robocopy.bat
+++ snippets/bat/robocopy.bat
@@ -1,5 +1,9 @@
 REM Copy local folder to remote folder
 REM I executed this batch on Windows 8.1
+REM
+REM Target folder had 'hidden' and 'system' attributes if drive (e.g. d:\) would be set as source folder
+REM Ref. https://social.technet.microsoft.com/Forums/ja-JP/eb3bc943-f249-4641-80fb-d378cb91d043/robocopy?forum=windowsserver2008ja (in Japanese)
+
 set /P answer="Do you synchronize your data to remote host? (y/n): "
 if /i "%answer%" == "y" (
 robocopy "C:\Users\sample\Documents\mydata" "\\192.168.0.120\backup\" /E /FFT /R:3 /W:5 /NP /TEE /LOG+:"\\192.168.0.120\backup\robocopy.log"
