e6e0abfe3937cce6b5703d2b2755457f86e5d79f
kaito834 <kaito834@users.noreply.github.com>
2017-04-16 21:10:03 +0900

    Added a batch file to set AWS_SECERT_ACCESS_KEY from KeePass kdbx file.

---
+++ snippets/bat/setAwsSecretFromKdbx.bat
@@ -0,0 +1,31 @@
+@echo off
+REM This batch file was tested on Windows 10
+
+set Exe_KPScript=C:\Users\kaito\Documents\mydata\KeePass\KPScript.exe
+set KdbxFile=C:\Users\kaito\Documents\mydata\kdbx\myDatabase.kdbx
+
+REM http://keepass.info/help/v2_dev/scr_sc_index.html#masterkey
+REM http://keepass.info/help/v2_dev/scr_sc_index.html#getentrystring
+set Cmd_KPScript=%Exe_KPScript% -guikeyprompt -c:GetEntryString "%KdbxFile%" -Field:Password -ref-Title:"aws_iam-kaito834-program"
+
+for /f "usebackq delims=" %%a in (`%Cmd_KPScript%`) do (
+  set Cmd_KPScript_Result=%%a
+  REM The output of %Cmd_KPScript% consists of two line, but only first line is necessary
+  REM https://technet.microsoft.com/en-us/library/bb490914.aspx
+  goto exit_loop
+)
+
+:exit_loop
+REM https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#cli-environment
+REM https://docs.aws.amazon.com/cli/latest/userguide/cli-http-proxy.html
+REM set HTTPS_PROXY and HTTP_PROXY if necessary
+set AWS_ACCESS_KEY_ID=xxx
+set AWS_SECRET_ACCESS_KEY=%Cmd_KPScript_Result%
+
+REM Unset some environment variables which are not necessary
+set Exe_KPScript=
+set KdbxFile=
+set Cmd_KPScript=
+set Cmd_KPScript_Result=
+
+REM pause
